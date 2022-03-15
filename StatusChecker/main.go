8bc0a3dd7a1bd8b97acbc890acb4411bb4228d2e
DavidG <dg_codemonkey@outlook.com>
2022-03-15 17:49:57 +0800

    Updated

---
+++ StatusChecker/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://www.google.com",
+		"http://www.facebook.com",
+		"http://www.stackoverflow.com",
+		"http://www.golang.org",
+		"http://www.amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func() {
+			time.Sleep(time.Second * 5)
+			checkLink(l, c)
+		}()
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, " might be down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up")
+	c <- link
+}
